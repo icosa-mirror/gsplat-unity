c401653b696987592627c86efadbad31d7ae3210
Andy Baker <andy@andybak.net>
2026-06-29 14:46:46 +0100

    Intersector helper for Open Brush SelectionWidget

--- Runtime/GsplatRenderer.cs
+++ Runtime/GsplatRenderer.cs
@@ -87,6 +87,16 @@ namespace Gsplat
 
         public void ComputeDepth(CommandBuffer cmd, Matrix4x4 matrixMv) => m_renderer.ComputeDepth(cmd, matrixMv);
 
+        public bool TryIntersectSphere(Vector3 centerWorld, float radiusWorld, out float score)
+        {
+            score = -1.0f;
+            if (!Valid || m_renderer == null)
+                return false;
+
+            return m_renderer.TryIntersectSphere(transform, centerWorld, radiusWorld,
+                1.0f - SplatDownscaleFactor, out score);
+        }
+
         void OnEnable()
         {
             GsplatSorter.Instance.RegisterGsplat(this);
