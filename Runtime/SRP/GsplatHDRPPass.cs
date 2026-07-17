aeb81b356182729cde0b83b401cc16948975ac81
Andy Baker <andy@andybak.net>
2026-07-17 15:44:51 +0100

    Write splat depth in HDRP

--- Runtime/SRP/GsplatHDRPPass.cs
+++ Runtime/SRP/GsplatHDRPPass.cs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 #if GSPLAT_ENABLE_HDRP
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 
 namespace Gsplat
@@ -13,6 +14,8 @@ namespace Gsplat
             if (GsplatSorter.Instance.Valid && GsplatSettings.Instance.Valid && GsplatSorter.Instance.GatherGsplatsForCamera(ctx.hdCamera.camera))
             {
                 GsplatSorter.Instance.DispatchSort(ctx.cmd, ctx.hdCamera.camera);
+                CoreUtils.SetRenderTarget(ctx.cmd, ctx.cameraColorBuffer, ctx.cameraDepthBuffer);
+                GsplatSorter.Instance.RenderDepthPrepass(ctx.cmd, ctx.hdCamera.camera);
                 GsplatSorter.Instance.RenderColor(ctx.cmd, ctx.hdCamera.camera);
             }
         }
